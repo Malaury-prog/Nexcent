4603b85aae8b0dc6a669217f52ddb179aa933693
Malo <Cousin.mal3@gmail.com>
2026-02-03 14:30:28 +0100

    section1/responsive

---
+++ component/header.php
@@ -0,0 +1,29 @@
+<header>
+    <div class="header_container">
+        <div class="header_logo">
+            <?php
+            require("./assets/svg/icons/logo-nexcent.html")
+            ?>
+            <span>Nexcent</span>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Feature</a></li>
+                <li><a href="#">Product</a></li>
+                <li><a href="#">Testimonial</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+        </nav>
+        <div class="header_button">
+            <button>Login</button>
+            <button>Sign up</button>
+        </div>
+        <div class="burger">
+            <?php
+            require("./assets/svg/icons/logo-burger.html")
+            ?>
+        </div>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -9,7 +9,26 @@
 </head>
 
 <body>
-
+    <?php require_once("./component/header.php") ?>
+    <main>
+        <section id="section_one" class="Hero">
+            <div class="container_left">
+                <h1>Lessons and insights <span>from 8 years</span></h1>
+                <p>Where to grow your business as a photographer: site or social media?</p>
+                <div class="group_element">
+                    <button>Register</button>
+                    <div class="circles">
+                        <div class="circle"></div>
+                        <div class="circle"></div>
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="container_illustration">
+                <img src="./assets/images/Illustration.png" alt="">
+            </div>
+        </section>
+    </main>
 </body>
 
 </html>
