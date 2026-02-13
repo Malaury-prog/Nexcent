207e7b4d69e7d84c00213c05116af20a0f8db4ed
Malo <Cousin.mal3@gmail.com>
2026-02-13 16:28:42 +0100

    update du corps HTML

--- index.php
+++ index.php
@@ -44,15 +44,21 @@
                     <div><?php
                             require("./assets/svg/icons/logo-sponsor-3.html")
                             ?></div>
-                    <div><?php
-                            require("./assets/svg/icons/logo-sponsor-4.html")
-                            ?></div>
-                    <div><?php
-                            require("./assets/svg/icons/logo-sponsor-5.html")
-                            ?></div>
-                    <div><?php
-                            require("./assets/svg/icons/logo-sponsor-6.html")
-                            ?></div>
+                    <div>
+                        <?php
+                        require("./assets/svg/icons/logo-sponsor-4.html")
+                        ?>
+                    </div>
+                    <div>
+                        <?php
+                        require("./assets/svg/icons/logo-sponsor-5.html")
+                        ?>
+                    </div>
+                    <div>
+                        <?php
+                        require("./assets/svg/icons/logo-sponsor-6.html")
+                        ?>
+                    </div>
                 </div>
             </div>
             <div class="community">
@@ -103,7 +109,48 @@
                         <p>Our membership management software provides full automation of membership renewals and payments</p>
                     </div>
                 </div>
-
+            </div>
+        </section>
+        <section>
+            <div>
+                <img src="" alt="">
+            </div>
+            <div>
+                <h3>The unseen of spending three years at pixelgrade</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
+                <button>Learn More</button>
+            </div>
+        </section>
+        <section>
+            <div>
+                <h3>helping a local <span>business reinvent itself</span></h3>
+                <p>We reached here with our hard work and dedication</p>
+            </div>
+            <div>
+                <div>svg
+                    <div>
+                        <p>2.245.341</p>
+                        <p>Members</p>
+                    </div>
+                </div>
+                <div>svg
+                    <div>
+                        <p>46,328</p>
+                        <p>Clubs</p>
+                    </div>
+                </div>
+                <div>svg
+                    <div>
+                        <p>828,867</p>
+                        <p>Event Bookings</p>
+                    </div>
+                </div>
+                <div>svg
+                    <div>
+                        <p>1.926.436</p>
+                        <p>Payements</p>
+                    </div>
+                </div>
             </div>
         </section>
     </main>
